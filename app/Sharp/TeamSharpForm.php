4e9c6a4e4c20f4997a884ad19383a45bb08a2563
Philippe Lonchampt <philippelonchampt@gmail.com>
2018-04-07 11:59:08 +0200

    Add titles in Team Form

--- app/Sharp/TeamSharpForm.php
+++ app/Sharp/TeamSharpForm.php
@@ -4,6 +4,7 @@ namespace App\Sharp;
 
 use App\Team;
 use Code16\Sharp\Form\Eloquent\WithSharpFormEloquentUpdater;
+use Code16\Sharp\Form\Fields\SharpFormListField;
 use Code16\Sharp\Form\Fields\SharpFormTextField;
 use Code16\Sharp\Form\Layout\FormLayoutColumn;
 use Code16\Sharp\Form\SharpForm;
@@ -20,6 +21,18 @@ class TeamSharpForm extends SharpForm
         )->addField(
             SharpFormTextField::make("country")
                 ->setLabel("Country")
+        )->addField(
+            SharpFormListField::make("titles")
+                ->setLabel("Titles")
+                ->setAddable()->setAddText("Add a title")
+                ->setRemovable()
+                ->addItemField(
+                    SharpFormTextField::make("competition")
+                        ->setLabel("Competition name")
+                )->addItemField(
+                    SharpFormTextField::make("title")
+                        ->setLabel("Title")
+                )
         );
     }
 
@@ -28,12 +41,16 @@ class TeamSharpForm extends SharpForm
         $this->addColumn(6, function(FormLayoutColumn $column) {
             $column->withSingleField("name")
                 ->withSingleField("country");
+        })->addColumn(6, function(FormLayoutColumn $column) {
+            $column->withSingleField("titles", function(FormLayoutColumn $item) {
+                $item->withFields("competition|7", "title|5");
+            });
         });
     }
 
     function find($id): array
     {
-        return $this->transform(Team::findOrFail($id));
+        return $this->transform(Team::with('titles')->findOrFail($id));
     }
 
     function update($id, array $data)
