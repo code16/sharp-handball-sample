4d56e4f7914feaad8f3bc4fffa1a7b18dfd1a1d0
Philippe Lonchampt <philippelonchampt@gmail.com>
2018-04-22 15:14:01 +0200

    Add data

--- app/Sharp/SetPlayerInjuredCommand.php
+++ app/Sharp/SetPlayerInjuredCommand.php
@@ -4,6 +4,8 @@ namespace App\Sharp;
 
 use App\Player;
 use Code16\Sharp\EntityList\Commands\InstanceCommand;
+use Code16\Sharp\Form\Fields\SharpFormTextareaField;
+use Code16\Sharp\Form\Layout\FormLayoutColumn;
 
 class SetPlayerInjuredCommand extends InstanceCommand
 {
@@ -20,9 +22,14 @@ class SetPlayerInjuredCommand extends InstanceCommand
      * @param string $instanceId
      * @param array $data
      * @return array
+     * @throws \Illuminate\Validation\ValidationException
      */
     public function execute($instanceId, array $data = []): array
     {
+        $this->validate($data, [
+            "detail" => "required"
+        ]);
+
         Player::findOrFail($instanceId)->setInjured(true);
 
         return $this->reload();
@@ -35,5 +42,18 @@ class SetPlayerInjuredCommand extends InstanceCommand
     public function authorizeFor($instanceId): bool
     {
         return !Player::findOrFail($instanceId)->injured;
+    }
+
+    public function buildFormFields()
+    {
+        return $this->addField(
+            SharpFormTextareaField::make("detail")
+                ->setLabel("Detail")
+        );
+    }
+
+    public function buildFormLayout(FormLayoutColumn &$column)
+    {
+        $column->withSingleField("detail");
     }
 }
